f17733c72da2da2309a4ebb45414ad91bbc2be21
abdelhalim LAKFIFI <62565296+abdelhalimlakfifi@users.noreply.github.com>
2023-11-20 12:42:02 +0100

    make store function

--- backend/app/Http/Controllers/CategoryController.php
+++ backend/app/Http/Controllers/CategoryController.php
@@ -3,6 +3,7 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use App\Models\Category;
 
 class CategoryController extends Controller
 {
@@ -16,6 +17,33 @@ class CategoryController extends Controller
     public function store(Request $request)
     {
 
-        dd($request->all());
+        // validates inputs
+        $validatedData = $request->validate([
+            'name' => 'required|unique:categories|max:255',
+        ]);
+
+        $parent = null;
+
+        if($request->parentName != null)
+        {
+            $parent = Category::where('name', $request->parentName)->first();
+
+
+            if(!$parent)
+            {
+                return response()->json(['message' => 'Category parent name not found'], 404);
+            }
+        }
+
+
+
+        $category = Category::create([
+            'name' => $request->name,
+            'parent_category_id'    =>  $parent != null ? $parent->id : null,
+        ]);
+
+
+        return response()->json($category, 201);
+
     }
 }
