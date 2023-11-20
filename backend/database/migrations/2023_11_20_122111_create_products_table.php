2ec27baa84242db95de643cc52554a0781fbb976
abdelhalim LAKFIFI <62565296+abdelhalimlakfifi@users.noreply.github.com>
2023-11-20 13:32:35 +0100

    make files for product and product categories

--- backend/database/migrations/2023_11_20_122111_create_products_table.php
+++ backend/database/migrations/2023_11_20_122111_create_products_table.php
@@ -11,11 +11,12 @@ return new class extends Migration
      */
     public function up(): void
     {
-        Schema::create('categories', function (Blueprint $table) {
+        Schema::create('products', function (Blueprint $table) {
             $table->id();
-            $table->string('name')->unique();
-            $table->unsignedBigInteger('parent_category_id')->nullable();
-            $table->foreign('parent_category_id')->references('id')->on('categories')->onDelete('cascade');
+            $table->string('name');
+            $table->text('description');
+            $table->float('price');
+            $table->text('image');
             $table->timestamps();
         });
     }
@@ -25,6 +26,6 @@ return new class extends Migration
      */
     public function down(): void
     {
-        Schema::dropIfExists('categories');
+        Schema::dropIfExists('products');
     }
 };
--- backend/routes/category.php
+++ backend/routes/category.php
@@ -12,4 +12,7 @@ Route::prefix('/categories')->group(function () {
 
     Route::get('/', [CategoryController::class, 'index'])->name('categories');
     Route::post('/store', [CategoryController::class, 'store'])->name('category.store');
+    Route::get('/{id}', [CategoryController::class, 'show'])->name('category.show');
+    Route::put('/{id}/update', [CategoryController::class, 'update'])->name('category.update');
+    Route::delete('/{id}/delete', [CategoryController::class, 'destroy'])->name('category.destroy');
 });
