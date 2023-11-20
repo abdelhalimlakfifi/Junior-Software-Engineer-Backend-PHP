634883ded265f87da7933ce0eb4cdbfa8065ae80
abdelhalim LAKFIFI <62565296+abdelhalimlakfifi@users.noreply.github.com>
2023-11-20 14:54:03 +0100

    make header and start building component

---
+++ frontend/src/components/Header.vue
@@ -0,0 +1,18 @@
+<script setup>
+
+</script>
+
+<template>
+    <div class=" flex w-full justify-center space-x-10 divide-x-4">
+        <div>
+            <img src="../assets/lakfifi.png" class=" w-32 h-28" alt="">
+        </div>
+        <div class=" items-center flex pl-10">
+            <img src="../assets/codnetwork.png" alt="">
+        </div>
+    </div>
+</template>
+
+<style scoped>
+
+</style>
