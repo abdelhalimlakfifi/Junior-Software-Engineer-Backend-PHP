7f2ee3204dcdf4fe15a7a67469c6bf8c2902baed
abdelhalim LAKFIFI <62565296+abdelhalimlakfifi@users.noreply.github.com>
2023-11-20 13:19:36 +0100

    implement show, update and destroy

--- backend/app/Http/Controllers/CategoryController.php
+++ backend/app/Http/Controllers/CategoryController.php
@@ -70,4 +70,81 @@ class CategoryController extends Controller
         }
 
     }
+
+
+
+    public function show($id)
+    {
+        try {
+            $category = Category::with('children')->find($id);
+
+            if (!$category) {
+                return response()->json(['message' => 'Category not found'], 404);
+            }
+
+            return response()->json($category, 200);
+        } catch (\Throwable $th) {
+            abort(500);
+        }
+    }
+
+
+
+    public function update(Request $request, $id)
+    {
+        $rules = [
+            'name' => 'required|unique:categories|max:255',
+        ];
+        $validator = Validator::make($request->all(), $rules);
+
+        if ($validator->fails()) {
+            return response()->json(['message' => $validator->errors()->first()], 422);
+        }
+
+        try {
+            $category = Category::find($id);
+
+            if (!$category) {
+                return response()->json(['message' => 'Category not found'], 404);
+            }
+
+            $parent = null;
+
+            if($request->parentName != null)
+            {
+                $parent = Category::where('name', $request->parentName)->first();
+
+                if(!$parent)
+                {
+                    return response()->json(['message' => 'Category parent name not found'], 404);
+                }
+            }
+
+            $category->update([
+                'name' => $request->name,
+                'parent_category_id' => $parent != null ? $parent->id : null,
+            ]);
+
+            return response()->json($category, 200);
+        } catch (\Throwable $th) {
+            abort(500);
+        }
+    }
+
+    public function destroy($id)
+    {
+        try {
+            $category = Category::find($id);
+
+            if (!$category) {
+                return response()->json(['message' => 'Category not found'], 404);
+            }
+
+            $category->delete();
+
+            return response()->json(['message' => 'Category deleted successfully'], 200);
+        } catch (\Throwable $th) {
+            abort(500);
+        }
+    }
 }
