384f535e72482288825484d3646f11d5c05d5a3c
abdelhalim LAKFIFI <62565296+abdelhalimlakfifi@users.noreply.github.com>
2023-11-20 21:59:55 +0100

    import sweet alert

--- frontend/src/main.js
+++ frontend/src/main.js
@@ -2,7 +2,11 @@ import { createApp } from 'vue'
 import { createRouter, createWebHistory } from 'vue-router'
 import './style.css'
 import App from './App.vue'
+import VueSweetalert2 from 'vue-sweetalert2';
+import 'sweetalert2/dist/sweetalert2.min.css';
 
+// If you don't need the styles, do not connect
+import 'sweetalert2/dist/sweetalert2.min.css';
 
 import Home from './Pages/Home.vue';
 import NewProduct from './Pages/NewProduct.vue';
@@ -15,6 +19,8 @@ const router = createRouter({
     ]
 });
 
+
 const app = createApp(App)
+app.use(VueSweetalert2);
 app.use(router);
 app.mount('#app')
