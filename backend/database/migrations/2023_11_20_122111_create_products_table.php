8821ce033e5d1b32068cf074621603c403476c74
abdelhalim LAKFIFI <62565296+abdelhalimlakfifi@users.noreply.github.com>
2023-11-20 14:20:19 +0100

    made image nullabel

--- backend/database/migrations/2023_11_20_122111_create_products_table.php
+++ backend/database/migrations/2023_11_20_122111_create_products_table.php
@@ -16,7 +16,7 @@ return new class extends Migration
             $table->string('name');
             $table->text('description');
             $table->float('price');
-            $table->text('image');
+            $table->text('image')->nullable();
             $table->timestamps();
         });
     }
